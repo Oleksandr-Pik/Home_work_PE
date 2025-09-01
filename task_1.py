37b4dbe557e8e71720f4c2840fc3aa42fbfe9405
Oleksandr <Valday@meta.ua>
2025-09-01 17:22:28 +0300

    created branch hw_2

--- task_1.py
+++ task_1.py
@@ -1,32 +1,10 @@
-# Створіть клас, який описує книгу.
-# Він повинен містити інформацію про автора, назву, рік видання та жанр.
-# Створіть кілька різних книжок. Визначте для нього методи _repr_ та _str_.
+# Створіть клас Editor, який містить методи view_document та edit_document.
+# Нехай метод edit_document виводить на екран інформацію про те,
+# що редагування документів недоступне для безкоштовної версії.
+# Створіть підклас ProEditor, у якому цей метод буде перевизначено.
+# Введіть ліцензійний ключ із клавіатури
+# і, якщо він коректний, створіть екземпляр класу ProEditor, інакше Editor.
+# Викликайте методи перегляду та редагування документів.
 
-class Book:
-    def __init__(self, author: str, title: str, year_pub: int, genre: str):
-        self.author = author
-        self.title = title
-        self.year_pub = year_pub
-        self.genre = genre
-
-    def __str__(self):
-        return f"Book (назва: {self.title}, автор: {self.author}, рік виданн: {self.year_pub}, жанр: {self.genre})"
-
-    def __repr__(self):
-        return f"Book({repr(self.title)}, {repr(self.author)}, {repr(self.year_pub)}, {repr(self.genre)})"
-
-
-book1 = Book(author="Дж. Толкін", title="Гобіт", year_pub=1937, genre="фентезі")
-book2 = Book(author="Джейн Остін", title="Гордість і упередження", year_pub=1813, genre="роман")
-book3 = Book(author="Джордж Орвелл", title="1984", year_pub=1949, genre="антиутопія")
-book4 = Book("Т. Шевченко", "Кобзар", 1832, "збірка віршів")
-
-print(book1)
-print(book2)
-print(book3)
-print(book4)
-print()
-print(repr(book1))
-print(repr(book2))
-print(repr(book3))
-print(repr(book4))
+class Editor:
+    pass
--- task_2.py
+++ task_2.py
@@ -1,34 +1,3 @@
-# Створіть клас, який описує відгук до книги.
-# Додайте до класу книги поле – список відгуків.
-# Зробіть так, щоб при виведенні книги на екран за допомогою функції print також виводилися відгуки до неї.
-
-class Review:
-    def __init__(self, review: str):
-        self.review = review
-
-
-class Book:
-    def __init__(self, author: str, title: str, year_pub: int, genre: str):
-        self.author = author
-        self.title = title
-        self.year_pub = year_pub
-        self.genre = genre
-        self.reviews = []
-
-    def __str__(self):
-        return f"Book (назва: {self.title}, автор: {self.author}, рік видання: {self.year_pub}, жанр: {self.genre}, відгуки: {self.reviews})"
-
-    def add_review(self, review: Review):
-        self.reviews.append(review)
-
-
-book1 = Book(author="Дж. Толкін", title="Гобіт", year_pub=1937, genre="фентезі")
-book2 = Book(author="Джейн Остін", title="Гордість і упередження", year_pub=1813, genre="роман")
-
-book1.add_review("Чудова книга")
-book1.add_review("Super")
-book1.add_review("Дуже затягнуто")
-book2.add_review("для тих, хто любить розмірковувати")
-
-print(book1)
-print(book2)
+# Опишіть класи графічного об'єкта, прямокутника та об'єкта, який може обробляти натискання миші.
+# Опишіть клас кнопки. Створіть об'єкт кнопки та звичайного прямокутника.
+# Викличте метод натискання на кнопку.
